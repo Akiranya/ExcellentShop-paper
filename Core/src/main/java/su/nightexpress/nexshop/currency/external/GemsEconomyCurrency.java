5742c327d692b68049eec20801e03ce9ec28d779
Akiranya <porridgen@gmail.com>
2023-06-28 23:47:32 +0800

    Add compatibility with GemsEconomy 2.0.0

--- Core/src/main/java/su/nightexpress/nexshop/currency/external/GemsEconomyCurrency.java
+++ Core/src/main/java/su/nightexpress/nexshop/currency/external/GemsEconomyCurrency.java
@@ -1,8 +1,9 @@
 package su.nightexpress.nexshop.currency.external;
 
-import me.xanium.gemseconomy.GemsEconomy;
-import me.xanium.gemseconomy.account.Account;
-import me.xanium.gemseconomy.currency.Currency;
+import me.xanium.gemseconomy.api.Account;
+import me.xanium.gemseconomy.api.Currency;
+import me.xanium.gemseconomy.api.GemsEconomy;
+import me.xanium.gemseconomy.api.GemsEconomyProvider;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 import su.nexmedia.engine.api.config.JYML;
@@ -18,7 +19,6 @@ import java.util.Locale;
  * This class should be instantiated with a currency identifier in GemsEconomy.
  */
 public class GemsEconomyCurrency extends AbstractCurrency implements MultiCurrency {
-
     private final String identifier;
 
     public GemsEconomyCurrency(@NotNull String identifier) {
@@ -31,17 +31,22 @@ public class GemsEconomyCurrency extends AbstractCurrency implements MultiCurren
         // we need to dynamically register an ICurrency for each currency in GemsEconomy database.
         // This also includes the dynamic creation of currency config files in ExcellentShop.
 
-        for (Currency currency : GemsEconomy.getInstance().getCurrencyManager().getCurrencies()) {
-            ShopAPI.getCurrencyManager().registerCurrency(new GemsEconomyCurrency(currency.getSingular()));
+        for (Currency currency : GemsEconomyProvider.get().getLoadedCurrencies()) {
+            ShopAPI.getCurrencyManager().registerCurrency(new GemsEconomyCurrency(currency.getName()));
         }
     }
 
     private static CurrencyConfig loadOrCreateConfig(String identifier) {
-        JYML jyml = JYML.loadOrExtract(ShopAPI.PLUGIN, CurrencyManager.DIR_DEFAULT
-                                                       + "gemseconomy~"
-                                                       + identifier.toLowerCase(Locale.ROOT)
-                                                       + ".yml");
-        if (!jyml.isSet("Name")) jyml.set("Name", "GemsEconomy : " + identifier.toUpperCase(Locale.ROOT));
+        JYML jyml = JYML.loadOrExtract(
+            ShopAPI.PLUGIN,
+            CurrencyManager.DIR_DEFAULT
+            + "gemseconomy~"
+            + identifier.toLowerCase(Locale.ROOT)
+            + ".yml"
+        );
+        if (!jyml.isSet("Name")) {
+            jyml.set("Name", "GemsEconomy : " + identifier.toUpperCase(Locale.ROOT));
+        }
         CurrencyConfig config = new CurrencyConfig(ShopAPI.PLUGIN, jyml);
         config.save();
         return config;
@@ -49,29 +54,32 @@ public class GemsEconomyCurrency extends AbstractCurrency implements MultiCurren
 
     @Override
     public double getBalance(@NotNull Player player) {
-        Currency gemsCurrency = GemsEconomy.getInstance().getCurrencyManager().getCurrency(this.identifier);
-        Account account = GemsEconomy.getInstance().getAccountManager().fetchAccount(player);
-        validateCurrency(gemsCurrency);
+        GemsEconomy economy = GemsEconomyProvider.get();
+        Currency currency = economy.getCurrency(identifier);
+        Account account = economy.getAccount(player.getUniqueId());
+        validateCurrency(currency);
         validateAccount(account, player);
-        return account.getBalance(gemsCurrency);
+        return account.getBalance(currency);
     }
 
     @Override
     public void give(@NotNull Player player, double amount) {
-        Currency gemsCurrency = GemsEconomy.getInstance().getCurrencyManager().getCurrency(this.identifier);
-        Account account = GemsEconomy.getInstance().getAccountManager().fetchAccount(player);
-        validateCurrency(gemsCurrency);
+        GemsEconomy economy = GemsEconomyProvider.get();
+        Currency currency = economy.getCurrency(identifier);
+        Account account = economy.getAccount(player.getUniqueId());
+        validateCurrency(currency);
         validateAccount(account, player);
-        account.deposit(gemsCurrency, amount);
+        account.deposit(currency, amount);
     }
 
     @Override
     public void take(@NotNull Player player, double amount) {
-        Currency gemsCurrency = GemsEconomy.getInstance().getCurrencyManager().getCurrency(this.identifier);
-        Account account = GemsEconomy.getInstance().getAccountManager().fetchAccount(player);
-        validateCurrency(gemsCurrency);
+        GemsEconomy economy = GemsEconomyProvider.get();
+        Currency currency = economy.getCurrency(identifier);
+        Account account = economy.getAccount(player.getUniqueId());
+        validateCurrency(currency);
         validateAccount(account, player);
-        account.withdraw(gemsCurrency, amount);
+        account.withdraw(currency, amount);
     }
 
     private <T> void validateAccount(T account, Player player) {
@@ -82,11 +90,12 @@ public class GemsEconomyCurrency extends AbstractCurrency implements MultiCurren
 
     private <T> void validateCurrency(T currency) {
         if (currency == null) {
-            throw new IllegalStateException("Cannot find GemsEconomy currency: " + this.identifier + ". "
-                                            + "You may have deleted a currency from the GemsEconomy database? "
-                                            + "Try to run command `/excellentshop reload` to load the "
-                                            + "currencies from GemsEconomy again.");
+            throw new IllegalStateException(
+                "Cannot find GemsEconomy currency: " + identifier + ". "
+                + "You may have deleted a currency from the GemsEconomy database? "
+                + "Try to run command `/excellentshop reload` to load the "
+                + "currencies from GemsEconomy again."
+            );
         }
     }
-
 }
